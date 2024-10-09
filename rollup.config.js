d47903a081417c9b57698a8ddaf3dcf7ca853abf
Kapo.Yang <kapo.yang@mirmiles.com>
2024-10-09 10:35:06 +0800

    feat: 分别导出ignoreAutoI18n

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,39 @@
 import typescript from 'rollup-plugin-ts'
 
 const packageNames = ['shared', 'vite-plugin', 'webpack-plugin',];
-const packages = packageNames.map((packageName,) => ({
-  input: `packages/${packageName}/index.ts`,
-  output: [
-    {
-      file: `./packages/${packageName}/dist/index.cjs`,
-      format: 'cjs',
-    },
-    {
-      file: `./packages/${packageName}/dist/index.js`,
-      format: 'es',
-    },
-  ],
-  external: ['path',],
-  plugins: [typescript(),],
-}),);
+const packages = packageNames.reduce((pkg, packageName,) => {
+  pkg.push({
+    input: `packages/${packageName}/index.ts`,
+    output: [
+      {
+        file: `./packages/${packageName}/dist/index.cjs`,
+        format: 'cjs',
+      },
+      {
+        file: `./packages/${packageName}/dist/index.js`,
+        format: 'es',
+      },
+    ],
+    external: ['path',],
+    plugins: [typescript(),],
+  },)
+  pkg.push({
+    input: `packages/${packageName}/utils.ts`,
+    output: [
+      {
+        file: `./packages/${packageName}/dist/utils.cjs`,
+        format: 'cjs',
+      },
+      {
+        file: `./packages/${packageName}/dist/utils.js`,
+        format: 'es',
+      },
+    ],
+    external: ['path',],
+    plugins: [typescript(),],
+  },)
+  return pkg
+}, [],);
 
 export default packages.concat([
   {
@@ -33,5 +51,4 @@ export default packages.concat([
     plugins: [typescript(),],
     external: ['path',],
   },
-
 ],)
